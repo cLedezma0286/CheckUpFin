d4221309242552946cb151e641db357573300922
DragonLatino <picheli@ciencias.unam.mx>
2018-12-13 16:39:18 -0600

    Change space size in files

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,11 +2,11 @@ import { Component } from "@angular/core";
 import { Router } from '@angular/router';
 
 @Component({
-    selector: 'home',
-    templateUrl: 'home.view.html',
-    styleUrls: ['home.style.css']
+  selector: 'home',
+  templateUrl: 'home.view.html',
+  styleUrls: ['home.style.css']
 })
 
 export class HomeComponent{
-    constructor( private router: Router){}
+  constructor( private router: Router){}
 }
