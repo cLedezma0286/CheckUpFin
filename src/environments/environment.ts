50fdba5258fa5227c5d55d0cf9d04255d6a1b87e
Javier Montiel <javiermontiel@Admins-Mac-mini.local>
2018-12-13 18:28:37 -0600

    Changed clients enviroment url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 const BASE_URL = 'http://private-47ba9c-checkupfinanciero1.apiary-mock.com/';
 export const environment = {
   production: false,
-  CLIENTS_URL: BASE_URL + 'clients',
+  CLIENTS_URL: BASE_URL + 'clientes',
   NOTES_URL: BASE_URL + 'notas',
   AGREEMENTS_URL: BASE_URL + 'acuerdos',
   INTERVIEWS_URL: BASE_URL + 'entrevistas',
