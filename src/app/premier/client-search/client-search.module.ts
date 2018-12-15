17573a997d14eaa953dd847e9fb0e6c26a35bff7
Javier Montiel <javiermontiel@Admins-Mac-mini.local>
2018-12-14 19:07:32 -0600

    Implemented search client functionality

--- src/app/premier/client-search/client-search.module.ts
+++ src/app/premier/client-search/client-search.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { ClientSearchComponent } from "./client-search.component";
-
 @NgModule({
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   declarations: [
     ClientSearchComponent
