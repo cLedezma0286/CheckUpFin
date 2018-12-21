3f3ed1841bfebfcf5215cf9da9e4dff10d775aad
Luis Javier Montiel Arenas <luisjavier.montielarenas@gmail.com>
2018-12-21 17:52:24 -0600

    Implemented menu layout and its show and hide functionality

--- src/app/premier/client-finances/header/header.component.ts
+++ src/app/premier/client-finances/header/header.component.ts
@@ -7,8 +7,17 @@ import { HeaderService } from '@shared-services/header.service';
 })
 export class HeaderComponent implements OnInit{
   percentage: number;
+  open_menu = false;
   constructor(public headerService: HeaderService){}
   ngOnInit() {
     this.headerService.currentPercentage.subscribe(percentage => this.percentage = percentage);
+  }
+  @HostListener('document:click', ['$event'])
+  closeHeaderMenuDOM(event){
+    if (event.target.getAttribute('open_menu')) {
+      this.open_menu = !this.show_menu;
+    }else{
+      this.open_menu = false;
+    }
   }
 }
