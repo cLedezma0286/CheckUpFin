55e84ef7a83b635e883c9092ae5f22b2e49ec2dc
Luis Javier Montiel Arenas <luisjavier.montielarenas@gmail.com>
2019-01-14 12:45:34 -0600

    Implemented new project flow Added links in the header

--- src/app/premier/client-finances/header/header.component.ts
+++ src/app/premier/client-finances/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, HostListener } from '@angular/core';
 import { HeaderService } from '@shared-services/header.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'header',
   templateUrl: 'header.view.html',
@@ -9,7 +10,7 @@ export class HeaderComponent implements OnInit{
   percentage: number;
   subtitle: string;
   open_menu = false;
-  constructor(public headerService: HeaderService){}
+  constructor(public headerService: HeaderService, public router: Router){}
   ngOnInit() {
     this.headerService.current_percentage.subscribe(percentage => this.percentage = percentage);
     this.headerService.current_subtitle.subscribe(subtitle => this.subtitle = subtitle);
@@ -21,5 +22,8 @@ export class HeaderComponent implements OnInit{
     }else{
       this.open_menu = false;
     }
+  }
+  goToRoute(route){
+    this.router.navigate([route]);
   }
 }
