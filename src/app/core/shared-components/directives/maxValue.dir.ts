76ae704b678a9b659437b790ed83716a470cc9b6
JomaKar <purpureoslossilencios@hotmail.com>
2019-04-23 18:07:02 -0500

    directiva maxValue aceptando ceros y e iniciando con strings vacíos si no hay data

--- src/app/core/shared-components/directives/maxValue.dir.ts
+++ src/app/core/shared-components/directives/maxValue.dir.ts
@@ -21,25 +21,29 @@ export class MaxValue implements OnInit{
     	// console.log('this.applyMaxValue ngOnInit', this.applyMaxValue);
     	if(!this.applyMaxValue) return;
 		let validNumber = this.textTransform(this.el.value); // opossite of transform
-		let theValue = (validNumber > 0) ? (validNumber.toString().length > 4 ? '$' + this.numberWithCommas(validNumber) : validNumber) : '';
+		let theValue = (validNumber > 0) ? (validNumber.toString().length > 3 ? '$' + this.numberWithCommas(validNumber) : validNumber) : '';
 		this.control.control.setValue(theValue);
 
 
 		this.control.valueChanges.subscribe(()  => {
 			// console.log('valueChanges', this.el.value);
 			if(!this.applyMaxValue) return;
-			let validNumber = this.textTransform(this.el.value); // opossite of transform
-			let theValue = (validNumber > 0) ? (validNumber.toString().length > 4 ? '$' + this.numberWithCommas(validNumber) : validNumber) : '';
+			let validNumber = this.textTransform(this.el.value); // opossite of transform(validNumber == 0 ? 0 : '');
+			let theValue = (validNumber.toString().length > 3) ? 
+								'$' + this.numberWithCommas(validNumber) 
+								: 
+								(validNumber.toString().length > 1 ? (validNumber > 0 ? validNumber : 0) : validNumber);
 			this.control.control.setValue(theValue, {emitEvent: false});
-		})
+		});
     }
 
 	textTransform(txt){
+		console.log('textTransform', txt);
 		txt = txt.replace(/\$/g, '');
 		txt = txt.replace(/,/g, '');
-		if(!this.maxValue) return txt;
+		if(!this.maxValue || txt.length == 0) return txt;
 		let text = (typeof txt == 'number') ? txt : Number(txt);
-		// console.log('textTransform', txt, 'Number(txt)', Number(txt));
+		console.log('textTransform', txt, 'txt.length', txt.length, 'Number(txt)', Number(txt), 'text', text);
 		return (text < this.maxValue) ? text : this.maxValue;
 	}
 }
